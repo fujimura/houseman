24817752ea116d04f191753cc4d57480162246a3
Fujimura Daisuke <me@fujimuradaisuke.com>
2016-05-06 12:41:46 +0900

    Add subcommand to show version

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,19 @@
 module Main where
 
 import           Data.Maybe
+import           Data.Version    (showVersion)
 import           Options.Generic
-import           System.Exit     (exitWith)
+import           System.Exit
 import           Text.Trifecta   (parseFromFile)
 
 import qualified Houseman
+import qualified Paths_houseman
 import qualified Procfile.Parse
+import           Procfile.Types
 
 data Option = Start { filename :: Maybe FilePath } |
-              Run   { command :: String, filename :: Maybe FilePath }
+              Run   { command :: String, filename :: Maybe FilePath } |
+              Version
               deriving (Generic, Show)
 
 instance ParseRecord Option
@@ -22,9 +26,14 @@ instance ParseRecord Option
 main :: IO ()
 main = do
     (opts :: Option) <- getRecord "Manage Procfile-based application"
-    mProcfile <- parseFromFile Procfile.Parse.procfile (fromMaybe "Procfile" $ filename opts)
+    case opts of
+      Start _       -> parseProcFile (fromMaybe "Procfile" $ filename opts) >>= Houseman.start >>= exitWith
+      Run {command} -> parseProcFile (fromMaybe "Procfile" $ filename opts) >>= Houseman.run command >>= exitWith
+      Version       -> putStrLn (showVersion Paths_houseman.version)
 
-    case (opts, mProcfile) of
-      (_, Nothing)                   -> return ()
-      (Start _, Just procfile)       -> Houseman.start procfile >>= exitWith
-      (Run {command}, Just procfile) -> Houseman.run command procfile >>= exitWith
+parseProcFile :: FilePath -> IO Procfile
+parseProcFile path = do
+  mProcfile <- parseFromFile Procfile.Parse.procfile path
+  case mProcfile of
+    Nothing -> exitWith (ExitFailure 1)
+    Just p -> return p
